3b6770a0f334d03430530a3d6461f554dc8febff
Stephen Aspden <98038352@live.blackburn.ac.uk>
2021-02-09 09:50:05 +0000

    Add files via upload

---
+++ Run a Git Hub Script.ps1
@@ -0,0 +1,2 @@
+﻿$ScriptFromGithHub = Invoke-WebRequest "https://raw.githubusercontent.com/Stephen136/Powershell/main/GetFileHash.ps1" -UseBasicParsing
+Invoke-Expression $($ScriptFromGithHub.Content)
