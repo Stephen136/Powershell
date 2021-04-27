733ec7b6d84bad6e7c568d11964b2b9d385fd056
Stephen Aspden <98038352@live.blackburn.ac.uk>
2021-04-27 23:39:10 +0100

    Update VboxVMStartHeadless.ps1

--- VboxVMStartHeadless.ps1
+++ VboxVMStartHeadless.ps1
@@ -1,4 +1,4 @@
-Function VboxVMStartHeadless(){
+Function VboxVmStartHeadless(){
   param([string]$vmID)
   $vmIDstr = "--startvm $vmID"
   start-process 'C:\program files\oracle\virtualbox\vboxheadless' $vmIDstr -WindowStyle Hidden
