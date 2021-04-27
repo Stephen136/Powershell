9227f5f2efb8beb1055e406e16c8ba7de73b6f7d
Stephen Aspden <98038352@live.blackburn.ac.uk>
2021-04-27 23:48:23 +0100

    Update and rename Run Git Script pass in args.ps1 to RunGITScriptPassInArgs.ps1

---
+++ RunGITScriptPassInArgs.ps1
@@ -0,0 +1,7 @@
+#script to run on local machine  
+param([String] $vmName = "VBOX VM NAME", [String] $vmID = "32946397-8b5a-4811-bb71-0c91fa499908") #VM UUID 
+
+    $scriptUrl = 'https://raw.githubusercontent.com/Stephen136/Powershell/main/VboxVmRestart.ps1'
+    . { iwr -useb  $scriptUrl } | iex; VboxVMRestart -vmName $vmName -vmID $vmID
+    
+    # Invoke-WebRequest (url) | Invoke-Expression;(URL FUNCTION NAME)_(Args; -vmName $vmName -vmID $vmID)
