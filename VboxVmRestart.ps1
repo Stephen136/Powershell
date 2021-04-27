0abdbf5dbe0e4002a7da0c7fe883fc5d82f592f3
Stephen Aspden <98038352@live.blackburn.ac.uk>
2021-04-27 23:42:05 +0100

    Update and rename VboxVMRestart.ps1 to VboxVmRestart.ps1

---
+++ VboxVmRestart.ps1
@@ -0,0 +1,11 @@
+Function VboxVmRestart(){
+    param([String] $vmName, [String] $vmID)
+
+    $scriptUrl = 'https://raw.githubusercontent.com/Stephen136/Powershell/main/VboxVmAcpiPowerdown.ps1'
+    . { iwr -useb  $scriptUrl } | iex; PowerdownVM -vmName $vmName
+
+        Start-Sleep -s 10
+
+    $scriptUrl = 'https://raw.githubusercontent.com/Stephen136/Powershell/main/VboxVmStartHeadless.ps1'
+    . { iwr -useb  $scriptUrl } | iex; VboxVMStartHeadless -vmID $vmID
+}
