2ddd739cf0fee5e326d61f2265474d99b586d9d8
Stephen Aspden <98038352@live.blackburn.ac.uk>
2021-04-28 15:00:05 +0100

    Update RunGitScriptPassInArgs.ps1

--- RunGitScriptPassInArgs.ps1
+++ RunGitScriptPassInArgs.ps1
@@ -2,6 +2,6 @@
 param([String] $vmName = "VBOX VM NAME", [String] $vmID = "32946397-8b5a-4811-bb71-0c91fa499908") #VM UUID 
 
     $scriptUrl = 'https://raw.githubusercontent.com/Stephen136/Powershell/main/VboxVmRestart.ps1'
-    . { iwr -useb  $scriptUrl } | iex; VboxVMRestart -vmName $vmName -vmID $vmID
+    . { iwr -useb  $scriptUrl } | iex; VboxVmRestart -vmName $vmName -vmID $vmID
     
-    # Invoke-WebRequest (url) | Invoke-Expression;(URL FUNCTION NAME)_(Args; -vmName $vmName -vmID $vmID)
+    # Invoke-WebRequest (url) | Invoke-Expression;(VboxVmRestart)_(Args; -vmName $vmName -vmID $vmID)
