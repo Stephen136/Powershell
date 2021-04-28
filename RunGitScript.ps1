bf1826ad4bcd21212ff52d820eba95f8f95f1836
Stephen Aspden <98038352@live.blackburn.ac.uk>
2021-04-28 15:00:51 +0100

    Update RunGitScript.ps1

--- RunGitScript.ps1
+++ RunGitScript.ps1
@@ -1,2 +1,2 @@
-﻿$ScriptFromGithHub = Invoke-WebRequest "https://raw.githubusercontent.com/Stephen136/Powershell/main/GetFileHash.ps1" -UseBasicParsing
-Invoke-Expression $($ScriptFromGithHub.Content)
+﻿$script = Invoke-WebRequest "https://raw.githubusercontent.com/Stephen136/Powershell/main/GetFileHash.ps1" -UseBasicParsing
+Invoke-Expression $($script.Content)
